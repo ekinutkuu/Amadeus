5d6f743a78e31dbb7187d4ccd12ba2c84f35d3dd
Ekin Utku <ekinutkugulmez@gmail.com>
2024-11-16 19:40:33 +0300

    end point configurations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+   return (
+      <Router>
+         <div className="App">
+            <Routes>
+               <Route path="/" element={<Login />} />
+               <Route path="/login" element={<Login />} />
+            </Routes>
+         </div>
+      </Router>
+   );
 }
 
 export default App;
